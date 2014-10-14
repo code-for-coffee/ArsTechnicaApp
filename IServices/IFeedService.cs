bacd6546d217da550a164e8985ceedbc33e01a87
James <jtraverjr@gmail.com>
2014-10-14 15:45:49 -0500

    scaffold

--- IServices/IFeedService.cs
+++ IServices/IFeedService.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Xml;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using ArsTechnicaApp.DomainObjects;
 
-namespace ArsTechnicaApp
+namespace ArsTechnicaApp.IServices
 {
 	/// <summary>
 	/// Handles all of the RSS Feed actions.
@@ -8,6 +14,36 @@ namespace ArsTechnicaApp
 	public interface IFeedService<T, U>
 	{
 		void GetFeed(T type);
-		U ParseFeed(T type);
+		List<U> ParseFeed(T type);
+	}
+
+	/// <summary>
+	/// Topic based feeds
+	/// </summary>
+	class TopicRssFeedService : IFeedService<ArsTopicFeeds, ArsArticle> 
+	{
+		void GetFeed(ArsTopicFeeds type) {
+			throw new NotImplementedException ("Not yet impl");
+		}
+		List<ArsArticle> ParseFeed(ArsTopicFeeds type){
+			var list = new List<ArsArticle> ();
+			throw new NotImplementedException ("Not yet impl");
+			return list;
+		}
+	}
+
+	/// <summary>
+	/// Section based feeds
+	/// </summary>
+	class SectionRssFeedService : IFeedService<ArsSection, ArsArticle>
+	{
+		void GetFeed(ArsSection type) {
+			throw new NotImplementedException ("Not yet impl");
+		}
+		List<ArsArticle> ParseFeed(ArsSection type){
+			var list = new List<ArsArticle> ();
+			throw new NotImplementedException ("Not yet impl");
+			return list;
+		}
 	}
 }
