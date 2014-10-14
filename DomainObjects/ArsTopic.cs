15af507f66417244abaf663b08d39ff706d62e23
James <jtraverjr@gmail.com>
2014-10-14 15:37:01 -0500

    Last of the DO's

--- DomainObjects/ArsTopic.cs
+++ DomainObjects/ArsTopic.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 
 namespace ArsTechnicaApp.DomainObjects
 {
-	public class ArsTopic : BaseDO
+	public class ArsTopicFeeds : BaseDO
 	{
+		ArsTopicFeeds() {
+			this.Topics.Add ("Apple");
+			this.Topics.Add ("Open Source");
+			this.Topics.Add ("Microsoft");
+			this.Topics.Add ("Google");
+			this.Topics.Add ("Security");
+			this.Topics.Add ("Telecom");
+			this.Topics.Add ("Patent");
+			this.Topics.Add ("Web");
 
+		}
 
+		ArsTopicFeeds(List<string> topics) {
+			this.Topics = topics;
+		}
+
+		public List<string> Topics { get; set; }
 	}
 }
 
